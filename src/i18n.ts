7e776b14e5f8efc4bfa57b99b9329a924f29d8d7
kaungmyatthu <kaungmyatthu692017@gmail.com>
2023-07-17 13:58:07 +0630

    fixed i18n errors

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,8 @@
-import { createI18n, LocaleMessages, VueMessageType } from 'vue-i18n';
-import { App, InjectionKey, Plugin } from 'vue';
+// @ts-ignore
+import { createI18n } from 'vue-i18n';
+// @ts-ignore
+import type { LocaleMessages, VueMessageType} from "vue-i18n"
+import type { App, InjectionKey, Plugin } from 'vue';
 
 // Define the injection key
 export const i18nKey: InjectionKey<Plugin> = Symbol('i18n');
@@ -8,21 +11,21 @@ export const i18nKey: InjectionKey<Plugin> = Symbol('i18n');
 import en from "@/locales/en.json";
 // @ts-ignore
 import ch from "@/locales/ch.json";
-function loadLocaleMessages():LocaleMessages<VueMessageType>{
-    const locales =[{en:en},{ch:ch}]
-    const messages:LocaleMessages<VueMessageType> = {};
-    locales.forEach(lang=>{
-        const key = Object.keys(lang)
-        messages[key[0]]= lang[key[0]]
-    })
-    return messages
-}
+// function loadLocaleMessages():LocaleMessages<VueMessageType>{
+//     const locales =[{en:en},{ch:ch}]
+//     const messages:LocaleMessages<VueMessageType> = {};
+//     locales.forEach(lang=>{
+//         const key = Object.keys(lang)
+//         messages[key[0]]= lang[key[0]]
+//     })
+//     return messages
+// }
 
 export const i18n=  createI18n({
     legacy: false,
     locale:'ch',
     fallbackLocale: 'en',
-    messages:loadLocaleMessages()
+    messages:{en,ch}
 })
 export function setupI18n(app: App) {
     app.use(i18n);
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import resolve from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-
   ],
   resolve: {
     alias: {
